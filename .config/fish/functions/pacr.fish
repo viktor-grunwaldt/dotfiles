55989401463b7cfd423b3c3596d58f50071a3cb0
Viktor Grünwaldt <vik777gru@gmail.com>
2023-05-29 17:50:21 +0200

    changes

--- .config/fish/functions/pacr.fish
+++ .config/fish/functions/pacr.fish
@@ -3,8 +3,8 @@ function pacr
         --header='Remove packages.
 <c-a>: all, <c-e>: explicit, <c-y>: aur-explicit' \
         --bind 'ctrl-a:change-prompt(all> )+reload(pacman -Qq)' \
-        --bind 'ctrl-e:change-prompt(exp> )+reload(pacman -Qe)' \
-        --bind 'ctrl-y:change-prompt(aur> )+reload(pacman -Qm)' \
+        --bind 'ctrl-e:change-prompt(exp> )+reload(pacman -Qqe)' \
+        --bind 'ctrl-y:change-prompt(aur> )+reload(pacman -Qqm)' \
         --multi --height=80% --preview 'paru -Si {1}' \
         --preview-window bottom | xargs -ro sudo pacman -Rsn
     pacman -Qqe >~/.config/pac-kages.txt
